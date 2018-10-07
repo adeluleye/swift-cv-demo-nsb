6c8e7e6a46684af2aa9acffcc6cddb074e489794
adeluleye <adelu4real@gmail.com>
2018-10-07 13:52:35 +0100

    remove sb, setup cv programmatically

--- CVDemoNSB/AppDelegate.swift
+++ CVDemoNSB/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let layout = UICollectionViewFlowLayout()
+        let rootViewController = ViewController(collectionViewLayout: layout)
+        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        window?.makeKeyAndVisible()
         return true
     }
 
--- CVDemoNSB/ViewController.swift
+++ CVDemoNSB/ViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView?.backgroundColor = .white
     }
-
-
 }
 
