672cd781900c2e2dac0637da6f02e6edef1d2708
adeluleye <adelu4real@gmail.com>
2018-10-07 16:14:17 +0100

    implement cv numberOfItemsInSection and cellForItemAt function

--- CVDemoNSB/ViewController.swift
+++ CVDemoNSB/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UICollectionViewController {
     
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,5 +22,15 @@ class ViewController: UICollectionViewController {
         
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, .font: UIFont.boldSystemFont(ofSize: 20)]
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
 }
 
